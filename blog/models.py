bdb28a6b2d6a37f5491c322095c038cb653b7a0c
Igor Degtiarov <igor.degtiarov@raccoongang.com>
2019-02-23 17:55:21 +0200

    New commit Last

--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,6 @@
 from django.conf import settings
 from django.db import models
+from django.db.models import ImageField
 from django.utils import timezone
 
 
@@ -7,6 +8,7 @@ class Post(models.Model):
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
     text = models.TextField()
+    image = models.TextField()
     created_date = models.DateTimeField(default=timezone.now)
     published_date = models.DateTimeField(blank=True, null=True)
 
